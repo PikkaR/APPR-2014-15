76c8d32c64c3a6b1ebea5788e4fd7839081b37aa
Pika Remškar <pika.remskar@gmail.com>
2014-12-03 12:15:26 +0100

    Uvozi tabel

--- lib/xml.r
+++ lib/xml.r
@@ -10,7 +10,7 @@ stripByPath <- function(x, path) {
 }
 
 uvozi.obcine <- function() {
-  url.obcine <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
+  url.obcine <- "http://en.wikipedia.org/wiki/List_of_Grand_Prix_motorcycle_racing_World_champions"
   doc.obcine <- htmlTreeParse(url.obcine, useInternalNodes=TRUE)
   
   # Poiščemo vse tabele v dokumentu
@@ -18,21 +18,26 @@ uvozi.obcine <- function() {
   
   # Iz druge tabele dobimo seznam vrstic (<tr>) neposredno pod
   # trenutnim vozliščem
-  vrstice <- getNodeSet(tabele[[2]], "./tr")
+  vrstice <- getNodeSet(tabele[[1]], "./tr")
   
   # Seznam vrstic pretvorimo v seznam (znakovnih) vektorjev
   # s porezanimi vsebinami celic (<td>) neposredno pod trenutnim vozliščem
   seznam <- lapply(vrstice[2:length(vrstice)], stripByPath, "./td")
+  imena <- unlist(lapply(vrstice[2:length(vrstice)], stripByPath, "./td/span[@class='sortkey']"))
   
   # Iz seznama vrstic naredimo matriko
   matrika <- matrix(unlist(seznam), nrow=length(seznam), byrow=TRUE)
   
   # Imena stolpcev matrike dobimo iz celic (<th>) glave (prve vrstice) prve tabele
-  colnames(matrika) <- gsub("\n", " ", stripByPath(tabele[[2]][[1]], ".//th"))
+  stolpci <- gsub("\n", " ", stripByPath(vrstice[[1]], ".//th"))
+  colnames(matrika) <- stolpci[-1]
+  
+  zmage <- strsplit(matrika[,"Winning span"], substr(matrika[1,"Winning span"], 5, 5), fixed = TRUE)
+  prva.zmaga <- sapply(zmage, function(x) x[[1]])
+  zadnja.zmaga <- sapply(zmage, function(x) x[[length(x)]])
+  
   
   # Podatke iz matrike spravimo v razpredelnico
-  return(data.frame(apply(gsub("\\*", "",
-                          gsub(",", ".",
-                          gsub("\\.", "", matrika[,2:5]))),
-                    2, as.numeric), row.names=matrika[,1]))
+  return(data.frame(Country = matrika[,"Country"], First.win = prva.zmaga, Last.win = zadnja.zmaga,
+                    apply(matrika[,4:9], 2, as.numeric), row.names=imena))
 }
--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,19 +1,30 @@
 # 2. faza: Uvoz podatkov
 
 # Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvoziDruzine <- function() {
-  return(read.table("podatki/druzine.csv", sep = ";", as.is = TRUE,
-                      row.names = 1,
-                      col.names = c("obcina", "en", "dva", "tri", "stiri"),
-                      fileEncoding = "Windows-1250"))
+uvoziWinnersByNationality <- function() {
+  return(read.table("podatki/WinnersByNationality.csv", sep = ",", as.is = TRUE,
+                      
+                      
+                      header = TRUE, fileEncoding = "Windows-1250"))
 }
 
 # Zapišimo podatke v razpredelnico druzine.
-cat("Uvažam podatke o družinah...\n")
-druzine <- uvoziDruzine()
+cat("Uvažam podatke o zmagovalcih po narodih...\n")
+WinnersByNationality <- uvoziWinnersByNationality()
 
 # Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
 # potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
 # datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
 # 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
 # fazah.
+
+uvoziWorldChampByCountry <- function() {
+  return(read.table("podatki/WorldChampByCountry.csv", sep = ",", as.is = TRUE,
+                    
+                    
+                    header = TRUE, fileEncoding = "Windows-1250"))
+}
+
+# Zapišimo podatke v razpredelnico druzine.
+cat("Uvažam podatke o svetovnih prvakih po državah...\n")
+WorldChampByCountry <- uvoziWorldChampByCountry()
