7ba179e8e88e49cd19749c40bd0c9babd3bcd6ba
Pika Remškar <pika.remskar@gmail.com>
2015-03-09 12:18:14 +0100

    Tabele za 4.fazo

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -44,3 +44,63 @@ Dirkalisca <- uvoziDirkalisca()
 
 cat("Uvažam podatke o svetovnih prvakih...\n")
 WorldChamps <- uvozi.motogp()
+
+
+
+uvoziStatistika2014 <- function() {
+  return(read.table("podatki/Statistika2014.csv", sep = ",", as.is = TRUE,
+                    
+                    header = TRUE, fileEncoding = "Windows-1250"))
+}
+
+# Zapišimo podatke v razpredelnico druzine.
+cat("Uvažam statistične podatke za leto 2014...\n")
+Statistika2014 <- uvoziStatistika2014()
+
+
+uvoziStatistika2013 <- function() {
+  return(read.table("podatki/Statistika2013.csv", sep = ",", as.is = TRUE,
+                    
+                    header = TRUE, fileEncoding = "Windows-1250"))
+}
+
+# Zapišimo podatke v razpredelnico druzine.
+cat("Uvažam statistične podatke za leto 2013...\n")
+Statistika2013 <- uvoziStatistika2013()
+
+
+
+
+uvoziStatistika2012 <- function() {
+  return(read.table("podatki/Statistika2012.csv", sep = ",", as.is = TRUE,
+                    
+                    header = TRUE, fileEncoding = "Windows-1250"))
+}
+
+# Zapišimo podatke v razpredelnico druzine.
+cat("Uvažam statistične podatke za leto 2012...\n")
+Statistika2012 <- uvoziStatistika2012()
+
+
+uvoziStatistika2011 <- function() {
+  return(read.table("podatki/Statistika2011.csv", sep = ",", as.is = TRUE,
+                    
+                    header = TRUE, fileEncoding = "Windows-1250"))
+}
+
+# Zapišimo podatke v razpredelnico druzine.
+cat("Uvažam statistične podatke za leto 2011...\n")
+Statistika2011 <- uvoziStatistika2011()
+
+
+uvoziStatistika2010 <- function() {
+  return(read.table("podatki/Statistika2010.csv", sep = ",", as.is = TRUE,
+                    
+                    header = TRUE, fileEncoding = "Windows-1250"))
+}
+
+# Zapišimo podatke v razpredelnico druzine.
+cat("Uvažam statistične podatke za leto 2010...\n")
+Statistika2010 <- uvoziStatistika2010()
+
+
