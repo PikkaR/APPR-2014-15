9b4e22128d1cdbc7d66c2df890f0c0002d190a4c
Pika Remškar <pika.remskar@gmail.com>
2015-03-16 12:48:36 +0100

    Podatki 4.faza - grafi za animacijo

--- analiza/analiza.r
+++ analiza/analiza.r
@@ -1,19 +1,76 @@
 # 4. faza: Analiza podatkov
 
 # Uvozimo funkcijo za uvoz spletne strani.
-source("lib/xml.r")
+#source("lib/xml.r")
 
 # Preberemo spletno stran v razpredelnico.
-cat("Uvažam spletno stran...\n")
-tabela <- preuredi(uvozi.obcine(), obcine)
+#cat("Uvažam spletno stran...\n")
+#tabela <- preuredi(uvozi.obcine(), obcine)
 
 # Narišemo graf v datoteko PDF.
-cat("Rišem graf...\n")
-pdf("slike/naselja.pdf", width=6, height=4)
-plot(tabela[[1]], tabela[[4]],
-     main = "Število naselij glede na površino občine",
-     xlab = "Površina (km^2)",
-     ylab = "Št. naselij")
+#cat("Rišem graf...\n")
+#pdf("slike/naselja.pdf", width=6, height=4)
+#plot(tabela[[1]], tabela[[4]],
+#     main = "Število naselij glede na površino občine",
+#     xlab = "Površina (km^2)",
+#     ylab = "Št. naselij")
+#dev.off()
+
+pdf("slike/Analiza_hitrosti.pdf")
+
+m <- apply(Hitrost, 1, function(x) as.numeric(x[1]))
+barplot(m, beside=TRUE, main ='Najvišja dosežena hitrost leta 2010', sub
+        ='Dirkališča', ylab = 'Najvišja dosežena hitrost', col = c('maroon1'),
+        names.arg=row.names(Hitrost), las=2, cex.names=0.5)
+
+m <- apply(Hitrost, 1, function(x) as.numeric(x[2]))
+barplot(m, beside=TRUE, main ='Najvišja dosežena hitrost leta 2011', sub
+        ='Dirkališča', ylab = 'Najvišja dosežena hitrost', col = c('darkgoldenrod1'),
+        names.arg=row.names(Hitrost), las=2, cex.names=0.5)
+
+m <- apply(Hitrost, 1, function(x) as.numeric(x[3]))
+barplot(m, beside=TRUE, main ='Najvišja dosežena hitrost leta 2012', sub
+        ='Dirkališča', ylab = 'Najvišja dosežena hitrost', col = c('magenta4'),
+        names.arg=row.names(Hitrost), las=2, cex.names=0.5)
+
+m <- apply(Hitrost, 1, function(x) as.numeric(x[4]))
+barplot(m, beside=TRUE, main ='Najvišja dosežena hitrost leta 2013', sub
+        ='Dirkališča', ylab = 'Najvišja dosežena hitrost', col = c('bisque'),
+        names.arg=row.names(Hitrost), las=2, cex.names=0.5)
+
+m <- apply(Hitrost, 1, function(x) as.numeric(x[5]))
+barplot(m, beside=TRUE, main ='Najvišja dosežena hitrost leta 2014', sub
+        ='Dirkališča', ylab = 'Najvišja dosežena hitrost', col = c('cadetblue1'),
+        names.arg=row.names(Hitrost), las=2, cex.names=0.5)
+dev.off()
+
+
+pdf("slike/Analiza_casa.pdf")
+
+m <- apply(Cas, 1, function(x) as.numeric(x[1]))
+barplot(m, beside=TRUE, main ='Najhitrejši dosežen čas leta 2010', sub
+        ='Dirkališča', ylab = 'Najhitrejši dosežen čas', col = c('maroon1'),
+        names.arg=row.names(Cas), las=2, cex.names=0.5)
+
+m <- apply(Cas, 1, function(x) as.numeric(x[2]))
+barplot(m, beside=TRUE, main ='Najhitrejši dosežen čas leta 2011', sub
+        ='Dirkališča', ylab = 'Najhitrejši dosežen čas', col = c('cyan4'),
+        names.arg=row.names(Cas), las=2, cex.names=0.5)
+
+m <- apply(Cas, 1, function(x) as.numeric(x[3]))
+barplot(m, beside=TRUE, main ='Najhitrejši dosežen čas leta 2012', sub
+        ='Dirkališča', ylab = 'Najhitrejši dosežen čas', col = c('beige'),
+        names.arg=row.names(Cas), las=2, cex.names=0.5)
+
+m <- apply(Cas, 1, function(x) as.numeric(x[4]))
+barplot(m, beside=TRUE, main ='Najhitrejši dosežen čas leta 2013', sub
+        ='Dirkališča', ylab = 'Najhitrejši dosežen čas', col = c('brown2'),
+        names.arg=row.names(Cas), las=2, cex.names=0.5)
+
+m <- apply(Cas, 1, function(x) as.numeric(x[5]))
+barplot(m, beside=TRUE, main ='Najhitrejši dosežen čas leta 2014', sub
+        ='Dirkališča', ylab = 'Najhitrejši dosežen čas', col = c('aquamarine'),
+        names.arg=row.names(Cas), las=2, cex.names=0.5)
 dev.off()
 
 
--- slike/GrafZmage.R
+++ slike/GrafZmage.R
@@ -21,7 +21,8 @@ row.names(Dolzina) <- Analiza2010$Circuit
 m <- apply(Dolzina, 1, function(x) as.numeric(x[1:5]))
 pdf("slike/Dolzina.pdf")
 barplot(m, beside=TRUE, main ='Dolžine dirkališč', sub
-        ='Dirkališča', ylab = 'Število zmag', col = c('maroon1','magenta4',rgb(0,0,1,0.5), rgb(1,0,0,0.5), rgb(0,1,0,0.5)),      
+        ='Dirkališča', ylab = 'Dolžina dirkališč', col = c('maroon1','magenta4',rgb(0,0,1,0.5), rgb(1,0,0,0.5), rgb(0,1,0,0.3)),
+        legend =c('Leto 2010','Leto 2011','Leto 2012','Leto 2013','Leto 2014'),
         names.arg=row.names(Dolzina), las=2, cex.names=0.5)
 dev.off()
 
@@ -32,18 +33,20 @@ row.names(Hitrost) <- Analiza2010$Circuit
 m <- apply(Hitrost, 1, function(x) as.numeric(x[1:5]))
 pdf("slike/Hitrost.pdf")
 barplot(m, beside=TRUE, main ='Najvišja dosežena hitrost', sub
-        ='Dirkališča', ylab = 'Najvišja dosežena hitrost', col = c('maroon1','magenta4',rgb(0,0,1,0.5), rgb(1,0,0,0.5), rgb(0,1,0,0.5)),      
+        ='Dirkališča', ylab = 'Najvišja dosežena hitrost', col = c('maroon1','magenta4',rgb(0,0,1,0.5), rgb(1,0,0,0.5), rgb(0,1,0,0.5)),
+        legend =c('Leto 2010','Leto 2011','Leto 2012','Leto 2013','Leto 2014'),
         names.arg=row.names(Hitrost), las=2, cex.names=0.5)
 dev.off()
 
 
-CaS <- data.frame(A2010=Analiza2010$Time, A2011=Analiza2011$Time,A2012=Analiza2012$Time, A2013=Analiza2013$Time, A2014=Analiza2014$Time)
+Cas <- data.frame(A2010=Analiza2010$Time, A2011=Analiza2011$Time,A2012=Analiza2012$Time, A2013=Analiza2013$Time, A2014=Analiza2014$Time)
 row.names(Cas) <- Analiza2010$Circuit
 
 
 m <- apply(Cas, 1, function(x) as.numeric(x[1:5]))
 pdf("slike/Cas.pdf")
 barplot(m, beside=TRUE, main ='Najhitrejši dosežen čas', sub
-        ='Dirkališča', ylab = 'Najhitrejši dosežen čas', col = c('maroon1','magenta4',rgb(0,0,1,0.5), rgb(1,0,0,0.5), rgb(0,1,0,0.5)),      
-        names.arg=row.names(Hitrost), las=2, cex.names=0.5)
+        ='Dirkališča', ylab = 'Najhitrejši dosežen čas', col = c('maroon1','magenta4',rgb(0,0,1,0.5), rgb(1,0,0,0.5), rgb(0,1,0,0.5)),
+        legend =c('Leto 2010','Leto 2011','Leto 2012','Leto 2013','Leto 2014'),
+        names.arg=row.names(Cas), las=2, cex.names=0.5)
 dev.off()
