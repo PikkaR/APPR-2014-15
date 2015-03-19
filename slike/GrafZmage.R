636693c5934dc4adea8ef04266fc68f31cbd2a6b
Pika Remškar <pika.remskar@gmail.com>
2015-03-19 12:18:47 +0100

    Dokončala poročilo

--- slike/GrafZmage.R
+++ slike/GrafZmage.R
@@ -19,7 +19,7 @@ row.names(Dolzina) <- Analiza2010$Circuit
 
 
 m <- apply(Dolzina, 1, function(x) as.numeric(x[1:5]))
-pdf("slike/Dolzina.pdf", paper="a4")
+pdf("slike/Dolzina.pdf")
 barplot(m, beside=TRUE, main ='Dolžine dirkališč', sub
         ='Dirkališča', ylab = 'Dolžina dirkališč', col = c('maroon1','magenta4',rgb(0,0,1,0.5), rgb(1,0,0,0.5), rgb(0,1,0,0.3)),
         legend =c('Leto 2010','Leto 2011','Leto 2012','Leto 2013','Leto 2014'),
@@ -30,8 +30,8 @@ Hitrost <- data.frame(A2010=Analiza2010$Speed.km.h., A2011=Analiza2011$Speed.km.
 row.names(Hitrost) <- Analiza2010$Circuit
 
 
-m <- apply(Hitrost, 1, function(x) as.numeric(x[1:5]))
-pdf("slike/Hitrost.pdf", paper="a4")
+m <- apply(Hitrost, 1, function(x) as.numeric(x[1:5])
+pdf("slike/Hitrost.pdf")
 barplot(m, beside=TRUE, main ='Najvišja dosežena hitrost', sub
         ='Dirkališča', ylab = 'Najvišja dosežena hitrost', col = c('maroon1','magenta4',rgb(0,0,1,0.5), rgb(1,0,0,0.5), rgb(0,1,0,0.5)),
         legend =c('Leto 2010','Leto 2011','Leto 2012','Leto 2013','Leto 2014'),
@@ -44,7 +44,7 @@ row.names(Cas) <- Analiza2010$Circuit
 
 
 m <- apply(Cas, 1, function(x) as.numeric(x[1:5]))
-pdf("slike/Cas.pdf", paper="a4")
+pdf("slike/Cas.pdf")
 barplot(m, beside=TRUE, main ='Najhitrejši dosežen čas', sub
         ='Dirkališča', ylab = 'Najhitrejši dosežen čas', col = c('maroon1','magenta4',rgb(0,0,1,0.5), rgb(1,0,0,0.5), rgb(0,1,0,0.5)),
         legend =c('Leto 2010','Leto 2011','Leto 2012','Leto 2013','Leto 2014'),
